fff69e38605e90179215f122266d9ca0c7320ee2
ShagunSG <shaguneghatak@gmail.com>
2023-12-15 02:49:32 +0530

    major processing code added

--- website_backend/home/views.py
+++ website_backend/home/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from collections import deque
-import django_pandas
+import django_pandas as pd
+import numpy as np
+import threading
+import time
+from time import sleep
+from threading import Lock
 
 # Create your views here.
 def index(request):
@@ -12,4 +17,68 @@ def index(request):
         production_size = request.POST.get('production_size')
     for i in range (int(number_of_jobs)):
         buffer = deque()
+    def completionTime(jobID, sequenceNumber):
+        time = np.random.normal(loc=machiningSequence[jobID][sequenceNumber][1],scale=0.0)
+        return time
+    lockList = []
+    bufferList = []
+    consumerState = []
+
+    for i in range(int(number_of_machines)):
+        lock = Lock()
+        lockList.append(lock)
+        consumerState.append(0)
+
+    for i in range(int(number_of_jobs)):
+        buffer = deque()
+        for j in range(int(SequenceLength[i])):
+            machineID = machiningSequence[i][j][0]
+            cTime = completionTime(i,j)
+            buffer.append((machineID, completionTime(i, j)))
+            bufferList.append(buffer)
+    
+    entity_id = []
+    product_id = []
+    start_time = []
+    end_time = []
+    time_elapsed = []
+
+    def entity(jobID, buffer):
+        while(len(buffer)):
+            item = buffer.popleft()
+            with lockList[item[0]]:
+                print(f"Entity {item[0] + 1} started consuming item {jobID + 1}")
+                iniTime = time.time()
+                sleep(item[1])
+                finTime = iniTime + item[1]
+                print(f"Entity {item[0] + 1} finished consuming item {jobID + 1}")
+                end_time.append(finTime)
+                start_time.append(iniTime)
+                TimeElapsed = (finTime-iniTime)
+                print(TimeElapsed)
+                time_elapsed.append(TimeElapsed)
+                product_id.append(jobID + 1)
+                entity_id.append(item[0] + 1)
+
+
+    # Create and start consumer threads
+    entityThreads = []
+    for i in range(int(number_of_jobs)):
+        t = threading.Thread(target = entity, args=(i,bufferList[i]))
+        entityThreads.append(t)
+        t.start()
+
+    # Wait for all threads to finish
+    for t in entityThreads:
+        t.join()
+
+    list_data = list(zip(entity_id,product_id,start_time,end_time,time_elapsed))
+    list_data
+    data = pd.DataFrame(list_data, columns = ['Entity ID', 'Product ID', 'Start Time', 'End Time', 'Time Elapsed'])
+    data
+
+    csvData = data.to_csv(index=False)
+    csvFilePath = 'data.csv'
+    with open(csvFilePath, 'w') as file:
+        file.write(csvData)
     return render(request, 'index.html')
