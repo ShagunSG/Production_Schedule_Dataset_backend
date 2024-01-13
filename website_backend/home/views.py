ccf387eb09b394914fb2098b2e7f79c7be24fa30
ShagunSG <shaguneghatak@gmail.com>
2024-01-13 16:50:26 +0530

    generating dataset

--- website_backend/home/views.py
+++ website_backend/home/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, HttpResponseRedirect
 from collections import deque
-import django_pandas as dpd
 import pandas as pd
 import numpy as np
 import threading
@@ -45,14 +44,11 @@ def index(request):
                     k += 1
                 elif (sequence.iloc[i,j] == '-'):
                     j += 1
-                j += 1
+                else:
+                    j += 1
             SequenceLength.append(len(buffer))
+            print(len(buffer))
             machiningSequence.append(buffer)
-            # if (bool(prod_size.iloc[i,1]>1)):
-            #     l=1
-            #     while (l<prod_size.iloc[i,1]):
-            #         machiningSequence.append(buffer)
-            #         l += 1
         def completionTime(jobID, sequenceNumber):
             time = np.random.normal(loc=machiningSequence[jobID][sequenceNumber][1],scale=0.0)
             return time
@@ -67,25 +63,13 @@ def index(request):
 
         for i in range(int(number_of_jobs)):
             buffer = deque()
+            print("i = ", i)
             for j in range(int(SequenceLength[i])):
-                # if (i == 0):
+                print("j = ", j)
                 machineID = machiningSequence[i][j][0]
                 buffer.append((machineID, completionTime(i, j)))
-                    # if (bool(prod_size.iloc[i,1]>1)):
-                    #     l=1
-                    #     while (l<prod_size.iloc[i,1]):
-                    #         buffer.append((machineID, completionTime(i, j)))
-                    #         l += 1
-                # elif (i > 0):
-                #     machineID = machiningSequence[i + prod_size.iloc[i-1,1] - 1][j][0]
-                #     cTime = completionTime(i + prod_size.iloc[i-1,1] - 1, j)
-                #     buffer.append((machineID, cTime))
-                #     if (bool(prod_size.iloc[i,1]>1)):
-                #         l=1
-                #         while (l<prod_size.iloc[i,1]):
-                #             buffer.append((machineID, completionTime(i + prod_size.iloc[i-1,1] - 1, j)))
-                #             l += 1
-                bufferList.append(buffer)
+                print(buffer)
+            bufferList.append(buffer)
         print(bufferList)
         entity_id = []
         product_id = []
@@ -93,13 +77,14 @@ def index(request):
         end_time = []
         time_elapsed = []
 
-        sTime = time.time()
         def entity(jobID, buffer):
             while(len(buffer)):
                 item = buffer.popleft()
+                global finTime
+                finTime = 0
                 with lockList[item[0]]:
                     print(f"Entity {item[0] + 1} started consuming item {jobID + 1}")
-                    iniTime = time.time() - sTime
+                    iniTime = finTime
                     sleep(item[1]/4)
                     finTime = iniTime + item[1]
                     print(f"Entity {item[0] + 1} finished consuming item {jobID + 1}")
@@ -110,20 +95,18 @@ def index(request):
                     time_elapsed.append(TimeElapsed)
                     product_id.append(jobID + 1)
                     entity_id.append(item[0] + 1)
-                    if (bool(prod_size.iloc[i,1]>1)):
+                    if (bool(prod_size.iloc[jobID,1]>1)):
                         l=1
-                        while (l<prod_size.iloc[i,1]):
+                        while (l<prod_size.iloc[jobID,1]):
                             entity_id.append(item[0] + 1)
                             product_id.append(jobID + 1)
                             iniTime = finTime
                             start_time.append(iniTime)
                             finTime = iniTime + item[1]
                             end_time.append(finTime)
-                            finTime += item[1]
                             TimeElapsed = (finTime-iniTime)
                             time_elapsed.append(TimeElapsed)
                             l += 1
-
 
         # Create and start consumer threads
         entityThreads = []
@@ -137,9 +120,7 @@ def index(request):
             t.join()
 
         list_data = list(zip(product_id,entity_id,start_time,end_time,time_elapsed))
-        list_data
         data = pd.DataFrame(list_data, columns = ['Product ID', 'Entity ID', 'Start Time', 'End Time', 'Time Elapsed'])
-        data
 
         csvData = data.to_csv(index=False)
         csvFilePath = 'data.csv'
